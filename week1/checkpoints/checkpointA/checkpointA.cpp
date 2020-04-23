c50b1bc1aed95c427a015eee5df6413201214edc
Kaden <kadenpayne@gmail.com>
2020-04-22 22:44:11 -0700

    first commit

---
+++ week1/checkpoints/checkpointA/checkpointA.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//Show "Hello World" on screen
+int main() 
+{
+    char fName;
+    char age;
+
+    cout << "Hello CS 165 World";
+    
+    cout << "What is your first name?: ";
+    cin  >> fName;
+    cout << endl;
+
+    cout << "What is your age?: ";
+    cin  >> age;
+
+    cout << "Your name is " + fname + ". You are " + age + " years old.\n";
+
+}
+)
