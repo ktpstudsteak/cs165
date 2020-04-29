3f903d9a753aa481e323f7685892e6bd21b049ad
Kaden <kadenpayne@gmail.com>
2020-04-29 00:47:07 -0700

    checkpoint 2 done?

--- template.cpp
+++
@@ -1,7 +0,0 @@
-/*********************************************************************** * Program: *    Assignment 10, Hello World *    Brother Helfrich, CS124 * Author: *    Sam Student * Summary: *    This program is designed to be the first C++ program you have ever *    written. While not particularly complex, it is often the most difficult *    to write because the tools are so unfamiliar. ************************************************************************/ 
- 
-#include <iostream> using namespace std; 
- 
-/********************************************************************** * Hello world on the screen ***********************************************************************/ int main() {    // display    cout << "Hello World\n"; 
- 
-   return 0; }
