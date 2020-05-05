b19abc54b87a2389c2c7e3ec82b0fa6e4c46c7a1
Kaden <kadenpayne@gmail.com>
2020-05-05 00:05:17 -0700

    ahh

--- week3/prove02/prove02.cpp
+++ week3/prove02/prove02.cpp
@@ -1,30 +1,82 @@
 /***********************************************************************
 * Program:
-*    Assignment 12, Income
-*    Brother Birch, CS 124
+*    Prove 02, Digital Forensics
+*    Brother Alvey, CS 165
 * Author:
 *    Kaden Payne
 * Summary: 
-*    <SUMMARY DESCRIPTION>
+*    This program will take a log and parse it for the correct timestamp.
 *
 *    Estimated:  0.75 hrs   
 *    Actual:     0.20 hrs
-*      <COMPLAIN ABOUT EMACS>
+*      
 ************************************************************************/
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
+
+struct Record 
+{
+   string username;
+   string fileName;
+   long   timestamp;
+};
+
+/**********************************************************************
+ * PROMPT FILE
+ * prompt user for log file
+ ***********************************************************************/
+string promptFile()
+{
+   string fileName;
+   cout << "Enter the access record file: ";
+   cin >> fileName;
+
+   return fileName;  
+}
+
+/**********************************************************************
+ * READ FILE
+ * read in the file provided by the user, store in array of Records
+ ***********************************************************************/
+void readFile(const string fileName)
+{
+   ifstream fin(fileName);
+   if (fin.fail())
+   {
+      cout << "File: "
+           << fileName
+           << " was not read properly. Please try again.\n";
+   }
+
+   Record records[] = {};
+
+   //loop into array
+  {
+     /* code */
+  }
+}
+
+/**********************************************************************
+ * PROMPT TIME
+ * prompt user for timestamp
+ ***********************************************************************/
+void promptTime()
+{
+
+}
 
 /**********************************************************************
  * <MAIN DESCRIPTION HERE>
  ***********************************************************************/
 int main()
-{
-   /*cout rules - ONLY KEEP IF NEEDED
-   cout.setf(ios::fixed);       //no sci not
-   cout.setf(ios::showpoint);   //always show decimal
-   cout.precision(2);           //always have 2 dec points.
-   */
-   
+{  
    return 0;
+
+   //array of structs
+   Record records[] = {};
+
 }
